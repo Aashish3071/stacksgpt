3aacefc38c062f869b4d1a99afccf1c1d4c9b875
Aashish Sharma <aarav3071@gmail.com>
2026-09-11 09:09:55 +0530

    feat: integrate Google Tag Manager (GTM-T9X6VDM7)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Source_Serif_4, Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
@@ -51,6 +52,8 @@ export async function generateMetadata(): Promise<Metadata> {
     verification: s.searchConsoleId ? { google: s.searchConsoleId } : undefined,
   };
 }
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-T9X6VDM7";
+
 export default async function RootLayout({
   children,
 }: {
@@ -78,7 +81,24 @@ export default async function RootLayout({
       className={`${serif.variable} ${sans.variable}`}
       suppressHydrationWarning
     >
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
       <body className="flex min-h-screen flex-col" suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <PrivacyControls settings={publicSettings}>
           <Navbar categories={categories} />
           <main className="flex-1">{children}</main>
